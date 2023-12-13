3bfc8063d4cfaab6a421f25b69b124943bb26db7
sabinopa <priasabino@gmail.com>
2023-12-13 15:36:02 -0300

    api: implementa controller de api de receitas

--- spec/request/apis/user_search_api_spec.rb
+++ spec/request/apis/user_search_api_spec.rb
@@ -24,7 +24,78 @@ feature 'Recipe API' do
       expect(json_response).to have_content 'Lanche'
       expect(json_response).to have_content '10 minutos'
       expect(json_response).to have_content 'hamburguer, pão de hambuerguer, queijo'
-      expect(json_response).to have_content 'Frite o hamburguer, coloque no pão, coma'
+    end
+
+    scenario 'by cook_time' do
+      user = create(:user, email: 'user@email.com', password: '123456', role: :user)
+      recipe_type = create(:recipe_type, name: 'Sobremesa')
+      create(:recipe, title: 'Manjar', cook_time: 60,
+                      recipe_type:,
+                      ingredients: 'leite condensado, leite, leite de coco',
+                      instructions: 'Misture tudo, leve ao fogo e mexa, leve a geladeira')
+      recipe_type = create(:recipe_type, name: 'Lanche')
+      create(:recipe, title: 'Hamburguer', cook_time: 10,
+                      recipe_type:,
+                      ingredients: 'hamburguer, pão de hamburguer, queijo',
+                      instructions: 'Frite o hamburguer, coloque no pão, coma')
+
+      get 'api/v1/recipes', params: { cook_time: '10'}
+      
+      expect(response.status).to eq 200
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq 2
+      expect(json_response).to have_content 'Hamburguer'
+      expect(json_response).to have_content 'Lanche'
+      expect(json_response).to have_content '10 minutos'
+      expect(json_response).to have_content 'hamburguer, pão de hambuerguer, queijo'
+    end
+
+    scenario 'by recipe_type' do
+      user = create(:user, email: 'user@email.com', password: '123456', role: :user)
+      recipe_type = create(:recipe_type, name: 'Sobremesa')
+      create(:recipe, title: 'Manjar', cook_time: 60,
+                      recipe_type:,
+                      ingredients: 'leite condensado, leite, leite de coco',
+                      instructions: 'Misture tudo, leve ao fogo e mexa, leve a geladeira')
+      recipe_type = create(:recipe_type, name: 'Lanche')
+      create(:recipe, title: 'Hamburguer', cook_time: 10,
+                      recipe_type:,
+                      ingredients: 'hamburguer, pão de hamburguer, queijo',
+                      instructions: 'Frite o hamburguer, coloque no pão, coma')
+
+      get 'api/v1/recipes', params: { title: 'Lanche'}
+      
+      expect(response.status).to eq 200
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq 2
+      expect(json_response).to have_content 'Hamburguer'
+      expect(json_response).to have_content 'Lanche'
+      expect(json_response).to have_content '10 minutos'
+      expect(json_response).to have_content 'hamburguer, pão de hambuerguer, queijo'
+    end
+
+    scenario 'by user email' do
+      user = create(:user, email: 'user@email.com', password: '123456', role: :user)
+      recipe_type = create(:recipe_type, name: 'Sobremesa')
+      create(:recipe, title: 'Manjar', cook_time: 60,
+                      recipe_type:,
+                      ingredients: 'leite condensado, leite, leite de coco',
+                      instructions: 'Misture tudo, leve ao fogo e mexa, leve a geladeira')
+      recipe_type = create(:recipe_type, name: 'Lanche')
+      create(:recipe, title: 'Hamburguer', cook_time: 10,
+                      recipe_type:,
+                      ingredients: 'hamburguer, pão de hamburguer, queijo',
+                      instructions: 'Frite o hamburguer, coloque no pão, coma')
+
+      get 'api/v1/recipes', params: { user_id.email: 'user@email.com' }
+      
+      expect(response.status).to eq 200
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq 2
+      expect(json_response).to have_content 'Hamburguer'
+      expect(json_response).to have_content 'Lanche'
+      expect(json_response).to have_content '10 minutos'
+      expect(json_response).to have_content 'hamburguer, pão de hambuerguer, queijo'
     end
   end
 end
